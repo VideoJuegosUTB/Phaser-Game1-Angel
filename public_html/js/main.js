9ec0457adcfff2c5a39d9c94e14d889ed6d27962
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-02-26 18:57:57 -0500

    Le agregamos fisica a nuestro personaje, y rebota

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -39,12 +39,26 @@ function create() {
     //  Now let's create two ledges
     var ledge = platforms.create(400, 400, 'ground');
     
-     ledge.body.immovable = true;
+    ledge.body.immovable = true;
 
     ledge = platforms.create(-150, 250, 'ground');
 
     ledge.body.immovable = true;
     
+    // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(player);
+    
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+    
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
 }
 
 function update() {
