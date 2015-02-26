bc10dc1cb30937ae9156348d33e31f4b3d48df52
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-02-26 18:38:34 -0500

    Cargamos un display

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,3 +5,16 @@
  */
 
 
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+
+
