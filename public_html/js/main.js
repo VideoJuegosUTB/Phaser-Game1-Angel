4e1d2eedfe7b74d6b8705104973ba79ee296f47b
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-02-26 19:06:45 -0500

    Agregamos movimientos a nuestro personaje

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -59,11 +59,45 @@ function create() {
     //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
+    
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
     //  Collide the player and the stars with the platforms
     game.physics.arcade.collide(player, platforms);
+    
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+    
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        player.animations.play('left');
+    } else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        // The maxim hight to jump the player
+        player.body.velocity.y = -350;
+    }
 }
 
 
